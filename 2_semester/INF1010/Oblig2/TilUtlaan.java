fe5e365172a160c79399b847febe852a794df612
aleksavaDesktop <Alek@vage.com>
2016-02-08 23:43:10 +0100

    Ferdig med Javadoc kommentering

--- 2_semester/INF1010/Oblig2/TilUtlaan.java
+++ 2_semester/INF1010/Oblig2/TilUtlaan.java
@@ -1,4 +1,24 @@
+/**The objects of all the classes that implements this interface will be able
+*to borrow away this object to a given person recognized by a String. The
+*object could also be returned.
+*
+*@author Aleksander Vaage
+*version 8. february 2016
+*/
+
 interface TilUtlaan {
+
+  /**
+  *The method that borrows away the object to the given person recognized
+  *by the String
+  *
+  *@param name The name of the loaner
+  */
   public void borrowAway(String name);
+
+  /**
+  *Returns the object. The variable borrowedAway will be changed so that it's
+  *clear that the object has been returned.
+  */
   public void returnObject();
 }
